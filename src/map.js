b455b41017a6ee5f1cb4f1efa20f0d0eb4a7ea3d
Elena Kharsova <elena.kharsova@gmail.com>
2023-05-20 23:09:45 +0200

    add map

--- src/map.js
+++ src/map.js
@@ -16,27 +16,24 @@ export function showMap(element, location) {
   //   zoom: 10,
   // };
 
-  const { YMapDefaultMarker, YMapDefaultMarkersLayer } = ymaps3.import(
-    "@yandex/ymaps3-markers@0.0.1"
-  );
+  // const { YMapDefaultMarker, YMapDefaultMarkersLayer } = ymaps3.import(
+  //   "@yandex/ymaps3-markers@0.0.1"
+  // );
 
   const map = new YMap(mapNode, { location: LOCATION }, [
-    // new YMapDefaultSchemeLayer(),
+    new YMapDefaultSchemeLayer(),
   ]);
   // });
-  map.addChild(new YMapDefaultSchemeLayer());
-  map.addChild(new YMapDefaultMarkersLayer());
-  map.addChild(
-    new YMapDefaultMarker({
-      coordinates: [location.latitude, location.longitude],
-    })
-  );
+  // map.addChild(new YMapDefaultSchemeLayer());
+  // map.addChild(new YMapDefaultMarkersLayer());
+  // map.addChild(
+  //   new YMapDefaultMarker({
+  //     coordinates: [location.latitude, location.longitude],
+  //   })
+  // );
   console.log("map", map);
   console.log("location", location);
-  // map.setLocation({
-  //   ...NEW_LOCATION,
-  //   duration: 1000,
-  // });
+  map.setLocation([37.623082, 55.75254]);
   //   const newCoordinates = [59.9386300, 30.3141300]; // New coordinates (St. Petersburg)
   // //   map.setCenter(newCoordinates);
 }
