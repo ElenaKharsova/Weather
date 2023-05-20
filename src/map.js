63e77c8ac338bdc5d5b95cd8f3f8a1f2a5baf6ad
Elena Kharsova <elena.kharsova@gmail.com>
2023-05-21 01:25:49 +0200

    add map

--- src/map.js
+++ src/map.js
@@ -4,7 +4,7 @@ export function showMap(element, location) {
   const mapNode = element.querySelector("#map");
   const LOCATION = {
     center: [location.longitude, location.latitude],
-    zoom: 15,
+    zoom: 12,
   };
 
   const map = new YMap(mapNode, { location: LOCATION });
